3c68e44f1eab468aef504702b41e15b9daa0f4ea
navdeep03gill <navdeep03gill@gmail.com>
2022-11-23 15:07:28 -0500

    added basic files

---
+++ subject.h
@@ -0,0 +1,18 @@
+#ifndef SUBJECT_H
+#define SUBJECT_H
+#include <vector>
+#include "observer.h"
+
+class Observer;
+
+class Subject {
+  std::vector<Observer*> observers;
+ public:
+  void attach(Observer *o);
+  void detach(Observer *o);
+  void notifyObservers();
+  
+  virtual ~Subject() = default;
+};
+
+#endif
